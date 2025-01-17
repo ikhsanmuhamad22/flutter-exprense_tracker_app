0896687e6670cb7df224246cde58363957a2b7e4
ikhsan <ikhsanmuhamad3885@gmail.com>
2025-01-17 19:38:46 +0700

    add: expenses item

--- lib/widget/expenses_list/expenses_list.dart
+++ lib/widget/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widget/expenses_list/expenses_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -9,7 +10,9 @@ class ExpensesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) => Text(expenses[index].title),
+      itemBuilder: (context, index) => ExpensesItem(
+        expense: expenses[index],
+      ),
     );
   }
 }
